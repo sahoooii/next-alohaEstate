3e8b8d5a90a7a0c879192d548acb92f05626c7e5
sahoooii <saho.chu@gmail.com>
2024-10-07 18:35:17 +0900

    Creating profile page

---
+++ app/profile/create/page.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@/components/ui/button';
+
+const createProfileAction = async (formData: FormData) => {
+	'use server';
+	const firstName = formData.get('firstName') as string;
+	console.log(firstName);
+};
+
+const CreateProfilePage = () => {
+	return (
+		<section>
+			<h1 className='text-2xl font-mono mb-8 capitalize'>new user</h1>
+			<div className='border p-8 rounded-md max-w-lg'>
+				<form action={createProfileAction}>
+					<Button type='button' size='lg'>
+						Create Profile
+					</Button>
+				</form>
+			</div>
+		</section>
+	);
+};
+
+export default CreateProfilePage;
