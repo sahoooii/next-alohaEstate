47037b3ae880baae8a2a0ca3085dfa0602ff4b5c
sahoooii <saho.chu@gmail.com>
2024-10-15 09:54:19 +0900

    Setting connect db and created profile data

--- utils/actions/ProfileActions.ts
+++ utils/actions/ProfileActions.ts
@@ -1,17 +1,48 @@
 'use server';
 
+import connectDB from '@/config/database';
+import Profile from '@/models/Profile';
 import { profileSchema } from '../schemas';
+import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
+import cloudinary from '@/config/cloudinary';
+import { redirect } from 'next/navigation';
 
 export const createProfileAction = async (
 	prevState: unknown,
 	formData: FormData
 ) => {
+	await connectDB();
+
 	try {
+		const user = await currentUser();
+		if (!user) throw new Error('Please login to create a profile');
+
 		const rawData = Object.fromEntries(formData);
 		const validatedFields = profileSchema.parse(rawData);
-		return { message: 'Profile created successfully' };
+
+		// username unique validate
+		const username = validatedFields.username;
+		const usernameExists = await Profile.findOne({ username });
+		if (usernameExists) throw new Error('Your username is already in use.');
+
+		const profileData = {
+			clerkId: user?.id,
+			email: user.emailAddresses[0].emailAddress,
+			profileImage: user.imageUrl ?? '',
+			...validatedFields,
+		};
+		const newProfile = new Profile(profileData);
+		await newProfile.save();
+
+		await clerkClient.users.updateUserMetadata(user.id, {
+			privateMetadata: {
+				hasProfile: true,
+			},
+		});
 	} catch (error) {
-		console.log(error);
-		return { message: 'You must contain at least 2 characters' };
+		return {
+			message: error instanceof Error ? error.message : 'An error occurred',
+		};
 	}
+	redirect('/');
 };
