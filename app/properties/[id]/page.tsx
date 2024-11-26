d8ee459261b54184adbd86a3813f9ff2ca0c3f4a
sahoooii <saho.chu@gmail.com>
2024-11-26 17:03:16 +0900

    Created BreadCrumps and Share Button

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,13 +1,44 @@
 import { fetchPropertyDetails } from '@/actions/PropertyAction';
+import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
+import BreadCrumps from '@/components/properties/BreadCrumps';
+import ShareButton from '@/components/properties/ShareButton';
 import { redirect } from 'next/navigation';
 
 const PropertyDetailsPage = async ({ params }: { params: { id: string } }) => {
 	const property = await fetchPropertyDetails(params.id);
-	// console.log('pop:',property);
-
+	// console.log('pop:', property[1]);
 	if (!property) redirect('/');
 
-	return <div>PropertyDetailsPage</div>;
+	const {
+		id: propertyId,
+		name,
+		tagline,
+		price,
+		category,
+		description,
+		images,
+		guests,
+		bedrooms,
+		beds,
+		baths,
+		amenities,
+	} = property[0];
+
+	const { firstName, lastName, username, email, profileImages } = property[1];
+
+	return (
+		<section className='container mt-8'>
+			<BreadCrumps name={name} link='/properties' title='All Properties' />
+			<header className='flex justify-between items-center mt-4'>
+				<h1 className='text-4xl font-bold capitalize'>{name}</h1>
+				<div className='flex items-center gap-x-4'>
+					<ShareButton name={name} propertyId={propertyId} />
+					<FavoriteToggleButton propertyId={propertyId} />
+				</div>
+			</header>
+			<h3 className='py-3 text-xl text-gray-500 capitalize'>{tagline}</h3>
+		</section>
+	);
 };
 
 export default PropertyDetailsPage;
