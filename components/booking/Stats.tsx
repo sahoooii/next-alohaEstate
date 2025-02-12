07b0fb036a3e32da5117b385b4905cb242e05268
sahoooii <saho.chu@gmail.com>
2025-02-12 18:43:20 +0900

    Edit README and bug fixed at bookings page of stats

--- components/booking/Stats.tsx
+++ components/booking/Stats.tsx
@@ -4,6 +4,7 @@ import { fetchBookingsStats } from '@/actions/BookingAction';
 
 const Stats = async () => {
 	const stats = await fetchBookingsStats();
+	
 	return (
 		<div className='mt-8 grid md:grid-cols-2 gap-4 lg:grid-cols-3'>
 			<StatsCards title='properties' value={stats.properties} />
