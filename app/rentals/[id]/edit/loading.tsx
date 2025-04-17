d9aac8ec04b0d23fd3e003563354170a35a6dda5
sahoooii <saho.chu@gmail.com>
2025-04-17 16:33:16 +0900

    Deleted Suspense to show loading

---
+++ app/rentals/[id]/edit/loading.tsx
@@ -0,0 +1,8 @@
+import { EditPropertyLoadingCard } from '@/components/card/LoadingCards';
+import React from 'react';
+
+const loading = () => {
+	return <EditPropertyLoadingCard />;
+};
+
+export default loading;
